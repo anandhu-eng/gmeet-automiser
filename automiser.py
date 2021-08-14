3e452c7e2a9f5d33adcdcda2edbc89d036b97a81
anandhu-eng <anandhukicks@gmail.com>
2021-08-14 08:43:06 +0530

    feature addition

--- automiser.py
+++ automiser.py
@@ -1,3 +1,4 @@
+####                IMPORT SECTION              ####
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
@@ -6,7 +7,9 @@ from selenium.webdriver.common.by import By
 import time
 import getpass
 from plyer import notification
-#the class of number of participants:uGOf1d
+from datetime import datetime
+####                END OF THE SECTION           ####
+
 
 def gotomeet(usersname, passwrd, meetcode, browser_name):
     ####        BROWSER DRIVER SELECTION        ####
@@ -39,14 +42,11 @@ def gotomeet(usersname, passwrd, meetcode, browser_name):
     driver.maximize_window()
     time.sleep(4)
     driver.find_element_by_class_name("cta-wrapper").click()
-    #driver.find_element_by_xpath("//a[normalize-space()='Sign in']").click()
     username=driver.find_element_by_xpath("//input[@id='identifierId']")
     username.send_keys(usersname)
     driver.find_element_by_xpath("//span[normalize-space()='Next']").click()
     time.sleep(4)
-    #driver.implicitly_wait(10)
     password=driver.find_element_by_name("password")
-    #password=driver.find_element_by_css_selector("input[type=password]")
     password.send_keys(passwrd)
     password.send_keys(Keys.ENTER)
     time.sleep(7)
@@ -56,13 +56,11 @@ def gotomeet(usersname, passwrd, meetcode, browser_name):
     time.sleep(5)
     mute=WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "ZB88ed")))
     mute.click()
-    #driver.find_element_by_class_name("ZB88ed").click()
     time.sleep(2)
     driver.find_element_by_xpath("//div[@class='IYwVEf HotEze nAZzG']//div[@class='oTVIqe BcUQQ']//*[local-name()='svg']").click()
     time.sleep(2)
     driver.find_element_by_css_selector('div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
     time.sleep(5)
-    #participants_beg=driver.find_element_by_class_name("uGOf1d").get_attribute("textContent")
     while(True):
         try:
             driver.find_element_by_class_name("uGOf1d")
@@ -74,8 +72,12 @@ def gotomeet(usersname, passwrd, meetcode, browser_name):
     participants_end=0
     while(True):
         time.sleep(10)
-        participants_end=int(driver.find_element_by_class_name("uGOf1d").get_attribute("textContent"))
-        print(participants_end)
+        try:
+            participants_end=int(driver.find_element_by_class_name("uGOf1d").get_attribute("textContent"))
+            print(participants_end)
+        except:
+            notification.notify(title="Gmeet automiser", message="Error! Might be due to gmeet ending without consent of program!", timeout=10)
+            return
         if(participants_beg-participants_end>7):
             driver.find_element_by_xpath("//button[@aria-label='Leave call']").click()
             title="GMeet Automiser"
@@ -93,10 +95,40 @@ def gotomeet(usersname, passwrd, meetcode, browser_name):
             pass
 
 print("##### WELCOME TO MEET AUTOMISER #####")
+period_code=[]
+start_time=[]
 
 browser_name=input("Enter the name of the browser in which you want to open Gmeet:").lower()
-
 usersname=input("Enter the username of your Gmail:")
 passwrd=getpass.getpass(prompt="Enter the password of your Gmail:")
-meetcode=input("Enter the meet code:")
-gotomeet(usersname, passwrd, meetcode,browser_name)
+
+####        SECTION FOR ENTERING THE PERIOD DETAILS         ####
+number_of_periods=int(input("Enter the number of period you have today:"))
+for i in range(number_of_periods):
+    period=input(("Enter the meetcode for {period_number} period:").format(period_number=i+1))
+    inp_time=input("Enter the time of the meet in hour:")
+    inp_time_min=input("Enter the time of meet in minutes:")
+    period_code.append(period)
+    start_time.append(inp_time)
+    start_time.append(inp_time_min)
+####                    SECTION END                         ####
+
+####        SECTION FOR LOGGING INTO PERIODS                ####
+while(len(period_code)!=0):
+    time_now=datetime.now().strftime("%H:%M")
+    lcltime="{h}:{m}".format(h=start_time[0],m=start_time[1])
+    
+    if(time_now==lcltime):
+        gotomeet(usersname, passwrd, period_code[0],browser_name)
+        period_code.remove(period_code[0])
+        start_time.remove(start_time[0])
+        start_time.remove(start_time[0])
+    else:
+        time.sleep(20)
+####                END OF THE SECTION                      ####
+
+####                PROGRAM END MESSAGE                     ####
+title="GMeet automiser"
+message="All the sheduled classes are over!! Take rest now"
+notification.notify(title=title, message=message, timeout=10)
+####                END OF THE SECTION                      ####
