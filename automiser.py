c88c49e9397ecda5789059aa4656a80106c7a61c
anandhu-eng <anandhukicks@gmail.com>
2021-09-06 09:03:07 +0530

    code styled

--- automiser.py
+++ automiser.py
@@ -8,45 +8,54 @@ import time
 import getpass
 from plyer import notification
 from datetime import datetime
+
 ####                END OF THE SECTION           ####
 
 
 def gotomeet(usersname, passwrd, meetcode, browser_name):
     ####        BROWSER DRIVER SELECTION        ####
-    if(browser_name=="firefox"):
+    if browser_name == "firefox":
         from webdriver_manager.firefox import GeckoDriverManager
+
         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    elif(browser_name=="chrome"):
+    elif browser_name == "chrome":
         from webdriver_manager.chrome import ChromeDriverManager
+
         driver = webdriver.Chrome(ChromeDriverManager().install())
-    elif(browser_name=="chromium"):
+    elif browser_name == "chromium":
         from webdriver_manager.chrome import ChromeDriverManager
         from webdriver_manager.utils import ChromeType
-        driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-    elif(browser_name=="ie"):
+
+        driver = webdriver.Chrome(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        )
+    elif browser_name == "ie":
         from webdriver_manager.microsoft import IEDriverManager
+
         driver = webdriver.Ie(IEDriverManager().install())
-    elif(browser_name=="edge"):
+    elif browser_name == "edge":
         from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
         driver = webdriver.Edge(EdgeChromiumDriverManager().install())
-    elif(browser_name=="opera"):
+    elif browser_name == "opera":
         from webdriver_manager.opera import OperaDriverManager
+
         driver = webdriver.Opera(executable_path=OperaDriverManager().install())
     else:
         print("ENTERED BROWSER NOT SUPPORTED!!!")
         return
     ####        END OF BROWSER DRIVER SELECTION        ####
 
-    #to get a website
+    # to get a website
     driver.get("https://meet.google.com/")
     driver.maximize_window()
     time.sleep(4)
     driver.find_element_by_class_name("cta-wrapper").click()
-    username=driver.find_element_by_xpath("//input[@id='identifierId']")
+    username = driver.find_element_by_xpath("//input[@id='identifierId']")
     username.send_keys(usersname)
     driver.find_element_by_xpath("//span[normalize-space()='Next']").click()
     time.sleep(4)
-    password=driver.find_element_by_name("password")
+    password = driver.find_element_by_name("password")
     password.send_keys(passwrd)
     password.send_keys(Keys.ENTER)
     time.sleep(7)
@@ -54,72 +63,89 @@ def gotomeet(usersname, passwrd, meetcode, browser_name):
     elem.send_keys(meetcode)
     elem.send_keys(Keys.RETURN)
     time.sleep(5)
-    mute=WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "ZB88ed")))
+    mute = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "ZB88ed"))
+    )
     mute.click()
     time.sleep(2)
-    driver.find_element_by_xpath("//div[@class='IYwVEf HotEze nAZzG']//div[@class='oTVIqe BcUQQ']//*[local-name()='svg']").click()
+    driver.find_element_by_xpath(
+        "//div[@class='IYwVEf HotEze nAZzG']//div[@class='oTVIqe BcUQQ']//*[local-name()='svg']"
+    ).click()
     time.sleep(2)
-    driver.find_element_by_css_selector('div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+    driver.find_element_by_css_selector("div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt").click()
     time.sleep(5)
-    while(True):
+    while True:
         try:
             driver.find_element_by_class_name("uGOf1d")
             break
         except:
             pass
 
-    participants_beg=int(driver.find_element_by_class_name("uGOf1d").get_attribute("textContent"))
-    participants_end=0
-    while(True):
+    participants_beg = int(
+        driver.find_element_by_class_name("uGOf1d").get_attribute("textContent")
+    )
+    participants_end = 0
+    while True:
         time.sleep(10)
         try:
-            participants_end=int(driver.find_element_by_class_name("uGOf1d").get_attribute("textContent"))
+            participants_end = int(
+                driver.find_element_by_class_name("uGOf1d").get_attribute("textContent")
+            )
             print(participants_end)
         except:
-            notification.notify(title="Gmeet automiser", message="Error! Might be due to gmeet ending without consent of program!", timeout=10)
+            notification.notify(
+                title="Gmeet automiser",
+                message="Error! Might be due to gmeet ending without consent of program!",
+                timeout=10,
+            )
             return
-        if(participants_beg-participants_end>7):
+        if participants_beg - participants_end > 7:
             driver.find_element_by_xpath("//button[@aria-label='Leave call']").click()
-            title="GMeet Automiser"
-            message="Meet ended!! closing the window in 10 seconds"
+            title = "GMeet Automiser"
+            message = "Meet ended!! closing the window in 10 seconds"
             notification.notify(title=title, message=message, timeout=10)
             time.sleep(10)
             driver.close()
             return
-            
+
         else:
-            participants_beg=participants_end
-    #print(participants)
-    if(browser_name=="chrome"):
-        while(True):
+            participants_beg = participants_end
+    # print(participants)
+    if browser_name == "chrome":
+        while True:
             pass
 
-print("##### WELCOME TO MEET AUTOMISER #####")
-period_code=[]
-start_time=[]
 
-browser_name=input("Enter the name of the browser in which you want to open Gmeet:").lower()
-usersname=input("Enter the username of your Gmail:")
-passwrd=getpass.getpass(prompt="Enter the password of your Gmail:")
+print("##### WELCOME TO MEET AUTOMISER #####")
+period_code = []
+start_time = []
+
+browser_name = input(
+    "Enter the name of the browser in which you want to open Gmeet:"
+).lower()
+usersname = input("Enter the username of your Gmail:")
+passwrd = getpass.getpass(prompt="Enter the password of your Gmail:")
 
 ####        SECTION FOR ENTERING THE PERIOD DETAILS         ####
-number_of_periods=int(input("Enter the number of period you have today:"))
+number_of_periods = int(input("Enter the number of period you have today:"))
 for i in range(number_of_periods):
-    period=input(("Enter the meetcode for {period_number} period:").format(period_number=i+1))
-    inp_time=input("Enter the time of the meet in hour:")
-    inp_time_min=input("Enter the time of meet in minutes:")
+    period = input(
+        ("Enter the meetcode for {period_number} period:").format(period_number=i + 1)
+    )
+    inp_time = input("Enter the time of the meet in hour:")
+    inp_time_min = input("Enter the time of meet in minutes:")
     period_code.append(period)
     start_time.append(inp_time)
     start_time.append(inp_time_min)
 ####                    SECTION END                         ####
 
 ####        SECTION FOR LOGGING INTO PERIODS                ####
-while(len(period_code)!=0):
-    time_now=datetime.now().strftime("%H:%M")
-    lcltime="{h}:{m}".format(h=start_time[0],m=start_time[1])
-    
-    if(time_now==lcltime):
-        gotomeet(usersname, passwrd, period_code[0],browser_name)
+while len(period_code) != 0:
+    time_now = datetime.now().strftime("%H:%M")
+    lcltime = "{h}:{m}".format(h=start_time[0], m=start_time[1])
+
+    if time_now == lcltime:
+        gotomeet(usersname, passwrd, period_code[0], browser_name)
         period_code.remove(period_code[0])
         start_time.remove(start_time[0])
         start_time.remove(start_time[0])
@@ -128,7 +154,7 @@ while(len(period_code)!=0):
 ####                END OF THE SECTION                      ####
 
 ####                PROGRAM END MESSAGE                     ####
-title="GMeet automiser"
-message="All the sheduled classes are over!! Take rest now"
+title = "GMeet automiser"
+message = "All the sheduled classes are over!! Take rest now"
 notification.notify(title=title, message=message, timeout=10)
 ####                END OF THE SECTION                      ####
--- cmd.py
+++ cmd.py
@@ -2,49 +2,55 @@
 import subprocess
 import os
 
-#installing a python package
+# installing a python package
 def install(package):
-    temp=subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-	# get the output of ping
+    temp = subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    # get the output of ping
     output = str(temp.communicate())
-		
-    output = output.split("\n")
-	
-    output = output[0].split('\\')
 
-	# variable to store the result
+    output = output.split("\n")
+
+    output = output[0].split("\\")
+
+    # variable to store the result
     res = []
 
     for line in output:
-    	res.append(line)
+        res.append(line)
 
-	# print the results
-    print('ping results: ')
-    print('\n'.join(res[len(res) - 3:len(res) - 1]))
+    # print the results
+    print("ping results: ")
+    print("\n".join(res[len(res) - 3 : len(res) - 1]))
     return res
-
 
 
 def apt_install(module_name):
     try:
-        temp=subprocess.check_call(['sudo apt install {}'.format(module_name)], shell=True, stdin=None, stdout=open(os.devnull,"wb"),executable="/bin/bash")
+        temp = subprocess.check_call(
+            ["sudo apt install {}".format(module_name)],
+            shell=True,
+            stdin=None,
+            stdout=open(os.devnull, "wb"),
+            executable="/bin/bash",
+        )
     #    output = str(temp.communicate())
-#		
-#        output = output.split("\n")
-#	
- #       output = output[0].split('\\')
-#
-#	    # variable to store the result
- #       res = []
-#
- #       for line in output:
-  #  	    res.append(line)
-#
-#	    # print the results
- #       print('ping results: ')
-  #      print('\n'.join(res[len(res) - 3:len(res) - 1]))
+    #
+    #        output = output.split("\n")
+    #
+    #       output = output[0].split('\\')
+    #
+    # 	    # variable to store the result
+    #       res = []
+    #
+    #       for line in output:
+    #  	    res.append(line)
+    #
+    # 	    # print the results
+    #       print('ping results: ')
+    #      print('\n'.join(res[len(res) - 3:len(res) - 1]))
     except subprocess.CalledProcessError as e:
         print(e.output)
 
-#install('selenium')
-apt_install('ffmpeg')
+
+# install('selenium')
+apt_install("ffmpeg")
